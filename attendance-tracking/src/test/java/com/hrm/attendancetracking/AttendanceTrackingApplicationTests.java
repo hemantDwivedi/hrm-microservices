a1f3898aca140127538fce3770a9337f7bcc4921
hemantDwivedi <hemantdwiwedi@gmail.com>
2024-05-24 20:29:28 +0530

    added attendance tracking service

---
+++ attendance-tracking/src/test/java/com/hrm/attendancetracking/AttendanceTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrm.attendancetracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceTrackingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
